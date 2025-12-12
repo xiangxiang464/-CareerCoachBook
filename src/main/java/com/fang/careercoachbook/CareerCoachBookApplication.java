62c69ebce5b100071c8496e50634080f9ff8e12e
fang <2627729817@qq.com>
2025-12-12 20:03:28 +0800

    init

---
+++ src/main/java/com/fang/careercoachbook/CareerCoachBookApplication.java
@@ -0,0 +1,13 @@
+package com.fang.careercoachbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerCoachBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerCoachBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fang/careercoachbook/CareerCoachBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fang.careercoachbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerCoachBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
